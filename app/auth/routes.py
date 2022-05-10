11a639928b949b6e5360d4eec6018877cb50fd38
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-10 09:37:05 +0300

    function to check errors

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -13,3 +13,14 @@ def register():
     form = Register()
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+    #Adds new instance of a user to the databse
+    
+    if form.validate_on_submit():
+        password_encrypt = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user_created = User(username=form.username.data,email=form.email.data,password=password_encrypt)
+        db.session.add(user_created)
+        db.session.commit()
+        flash(f"Account created for {form.username.data}")
+        return redirect(url_for('users.login'))
+    
+    #Checks if form has errors
