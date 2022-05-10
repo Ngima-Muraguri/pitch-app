2fe1cc266666d6c0af2144ad880179c35a2eaef8
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-05-10 09:25:19 +0300

    class register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,3 +4,17 @@ from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from flask_login import  current_user
 from app.models import User
+
+class Register(FlaskForm):
+    #Validate not in database
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Thats username is taken. Please choose a different one")
+    
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:  
+            raise ValidationError('That email is taken. Please choose a different one')
+        
